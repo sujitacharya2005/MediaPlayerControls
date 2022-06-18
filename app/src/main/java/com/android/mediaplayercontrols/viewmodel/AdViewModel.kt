e962cc92a9916ef1d5887b747bc15879aa4b122d
Sujit Acharya <sujitacharya2005@gmail.com>
2022-06-18 16:50:06 +0530

    move to coroutine

--- app/src/main/java/com/android/mediaplayercontrols/viewmodel/AdViewModel.kt
+++ app/src/main/java/com/android/mediaplayercontrols/viewmodel/AdViewModel.kt
@@ -10,18 +10,19 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 /**
- * This ViewModel holds all Ad related data, communicates Fragment
- * This one ViewModel shares data between two fragments AdDetailsFragment and AdFragment
+ * This ViewModel holds all Ad related data, communicates Fragment This
+ * one ViewModel shares data between two fragments AdDetailsFragment and
+ * AdFragment
  *
  * @property adListGetUseCase
  */
 class AdViewModel(private val adListGetUseCase: AdListGetUseCase) : ViewModel() {
     private val _listOfAdsLiveData = MutableLiveData<List<Ad>>()
-    val listOfAdsLiveData:LiveData<List<Ad>>
+    val listOfAdsLiveData: LiveData<List<Ad>>
         get() = _listOfAdsLiveData
 
     private val _selectedItemLiveData = MutableLiveData<Int>()
-    val selectedItemLiveData:LiveData<Int>
+    val selectedItemLiveData: LiveData<Int>
         get() = _selectedItemLiveData
 
 
@@ -32,16 +33,19 @@ class AdViewModel(private val adListGetUseCase: AdListGetUseCase) : ViewModel() 
     }
 
     fun itemClicked(item: Ad, position: Int) {
-        //previous item is same or not 1st time update
-        if (_selectedItemLiveData.value != null ) {
-            val previousSelectedPosition = _selectedItemLiveData.value!!
-            val previousSelectedData = _listOfAdsLiveData.value?.get(_selectedItemLiveData.value!!)
-            previousSelectedData?.isSelected = false
-            _selectedItemLiveData.value =  previousSelectedPosition
+        viewModelScope.launch {
+            //previous item is same or not 1st time update
+            if (_selectedItemLiveData.value != null) {
+                val previousSelectedPosition = _selectedItemLiveData.value!!
+                val previousSelectedData =
+                    _listOfAdsLiveData.value?.get(_selectedItemLiveData.value!!)
+                previousSelectedData?.isSelected = false
+                _selectedItemLiveData.value = previousSelectedPosition
+            }
+            //update current one
+            item.isSelected = true
+            _selectedItemLiveData.value = position
         }
-        //update current one
-        item.isSelected = true
-        _selectedItemLiveData.value = position
     }
 
 }
